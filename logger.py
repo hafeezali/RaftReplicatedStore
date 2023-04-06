4bc877a072b0d6912811a10ab2da1ad915cd8f98
Sunaina Krishnamoorthy <sunaina@Sunainas-MBP.lan>
2023-04-05 22:06:05 -0500

    Adding server id to log format

--- logger.py
+++ logger.py
@@ -5,19 +5,20 @@ import os
 class Logging:
     def __init__(self, server_id):
         self.log_level = logging.DEBUG
-        self.logger = logging.getLogger()
+        # Logger name is set to server id
+        self.logger = logging.getLogger(server_id)
 
         log_folder_path = '/home/logs_dir'
         os.path.dirname(log_folder_path)
         if not os.path.exists(log_folder_path):
             os.makedirs(log_folder_path)
         self.log_file_name = f'{log_folder_path}/{server_id}_exec_logs.txt'
-
+        
 
     def get_logger(self):
         logging.basicConfig(filename=self.log_file_name, 
                             level=self.log_level,
-                            format='%(asctime)s : %(message)s', 
+                            format='%(asctime)s : %(name)s : %(message)s', 
                             datefmt='%d-%b-%y %H:%M:%S',
                             filemode='w')
 
